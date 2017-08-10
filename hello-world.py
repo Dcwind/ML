2f392ee086a86976b5e417423dff4dfbe0c37c5b
decent <danielekwuazi@gmail.com>
2017-08-10 17:37:52 +0700

    fixed py3

--- hello-world.py
+++ hello-world.py
@@ -1,8 +1,8 @@
-from sklearn import tree 
+from sklearn import tree
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
 
 # in labels 0 for apples and 1 for oranges
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print clf.predict([[160, 0]])
+print(clf.predict([[160, 0]]))
