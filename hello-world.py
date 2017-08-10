2afc1398bf14a0a32fb330a0ec93e443a44e3f51
decent <danielekwuazi@gmail.com>
2017-08-10 17:20:38 +0700

    classifier

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,8 @@
-import sklearn
+import sklearn import tree
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+
+# in labels 0 for apples and 1 for oranges
+classifier = tree.DecisionTreeClassif()
+classifier = classifier.fit(features, labels)
+
