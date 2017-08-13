77884c3fb9803a6bd11ccd5b4eda3da182f33e9e
decent <danielekwuazi@gmail.com>
2017-08-13 09:32:20 +0700

    training data

--- iris.py
+++ iris.py
@@ -3,4 +3,10 @@ from sklearn.datasets import load_iris
 from sklearn import tree
 
 iris = load_iris()
-print(iris.feature_names,iris.target_names)
+test_idx = [0, 50, 100]
+
+# training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.target, test_idx, axis=0)
+
+#
