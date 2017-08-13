6bd7721d6cf48fc549a04487086192539f7ee94f
decent <danielekwuazi@gmail.com>
2017-08-13 22:31:20 +0700

    itirate throught iris dataset

--- iris.py
+++ iris.py
@@ -5,6 +5,10 @@ from sklearn import tree
 iris = load_iris()
 test_idx = [0, 50, 100]
 
+#display dataset
+for i  in range(len(iris.target)):
+    print("Example %d:  label %s, features %s" % (i, iris.data[i], iris.target[i]))
+
 # training data
 train_target = np.delete(iris.target, test_idx)
 train_data = np.delete(iris.data, test_idx, axis=0)
@@ -20,3 +24,4 @@ clf = clf.fit(train_data, train_target)
 # test
 print(test_target)
 print(clf.predict(test_data))
+
