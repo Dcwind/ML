00a6304147731c774c18ce2c95f5f0e24095a8b2
decent <danielekwuazi@gmail.com>
2017-08-14 23:25:47 +0700

    cross_validation deprecated

--- pipeline-iris.py
+++ pipeline-iris.py
@@ -6,7 +6,7 @@ x = iris.data
 y = iris.target
 
 # split train and test dataset
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .5)
 
 # import classifier
