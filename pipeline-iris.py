08cd3fde6b398117bd5eb6fc7c2cd66d5a2efe1b
decent <danielekwuazi@gmail.com>
2017-08-14 23:16:53 +0700

    train classifier

--- pipeline-iris.py
+++ pipeline-iris.py
@@ -8,3 +8,12 @@ y = iris.target
 # split train and test dataset
 from sklearn.cross_validation import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .5)
+
+# import classifier
+from sklearn import tree
+my_classifier =  tree.DecisionTreeClassifier()
+
+# train classifier
+my_classifier.fit(x_train, y_train)
+
+predictions =  my_classifier.predict(x_test)
