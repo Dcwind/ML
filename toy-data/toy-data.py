5ce27c137eaea9ca3ddf0f111b5733a1ac9eccdc
decent <danielekwuazi@gmail.com>
2017-08-14 16:33:50 +0700

    generate toy data set

--- toy-data/toy-data.py
+++ toy-data/toy-data.py
@@ -1,3 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# toy dataset
+greyhounds = 500
+labs = 500
+
+# generate random height of greys and labs
+grey_height = 28 + 4  * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
