a65b6d55023a2c7d14784fbf4850c2a1931a9907
decent <danielekwuazi@gmail.com>
2017-08-14 23:08:46 +0700

    split train and test dataset

--- pipeline-iris.py
+++ pipeline-iris.py
@@ -1,3 +1,9 @@
 # import dataset
 from sklearn import datasets
-iris = datasets.load_iris
+iris = datasets.load_iris()
+
+x = iris.data
+y = iris.target
+
+# split train and test dataset
+from sklearn.cross_validation import train_test_split
