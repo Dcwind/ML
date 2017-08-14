4f89afa7a5d40131c96ed3c79370b8e3c16ab056
decent <danielekwuazi@gmail.com>
2017-08-14 16:36:56 +0700

    plot and show histogram

--- toy-data/toy-data.py
+++ toy-data/toy-data.py
@@ -8,3 +8,7 @@ labs = 500
 # generate random height of greys and labs
 grey_height = 28 + 4  * np.random.randn(greyhounds)
 lab_height = 24 + 4 * np.random.randn(labs)
+
+# plot a histogram
+plt.hist([grey_height, lab_height], stacked=True, color=['r','b'])
+plt.show()
