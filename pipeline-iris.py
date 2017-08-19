30db4c8a67f4a81cbcecbf141fd847aff369922f
decent <danielekwuazi@gmail.com>
2017-08-19 23:02:51 +0700

    implemented

--- pipeline-iris.py
+++ pipeline-iris.py
@@ -15,9 +15,16 @@ class ScrappyKKN():
             label = self.closest(row)
             predictions.append(label)
         return predictions
+
     def closest(self, row):
         best_dist =  euc(row, self.x_train[0])
         best_index = 0
+        for i in range(1, len(self.x_train)):
+            dist = euc(row, self.x_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
 
 # import dataset
 from sklearn import datasets
